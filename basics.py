39a66938c725814b7ca42f53e4fbb3a242d89dc1
Trosn <110437947+Trosn@users.noreply.github.com>
2023-06-18 01:59:12 +1000

    regression polynomial
    regression model based on dates and CLOSE prices for each day.
    Polynomial of degree 4. Subject to change

--- basics.py
+++ basics.py
@@ -2,6 +2,12 @@ import requests
 import pandas as pd
 from config import TIINGO_API_KEY
 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import metrics
 
 # NASDAQ abbreviation for stock
 symbol = 'NVDA'
@@ -25,3 +31,30 @@ if response.status_code == 200:
     print(data)
 else:
     print(f"Failed to get data from Tiingo API. Status code: {response.status_code}. Response: {response.content}")
+
+df = data
+df['date'] = pd.to_datetime(df['date'])
+df['date_delta'] = (df['date'] - df['date'].min())  / np.timedelta64(1,'D')
+
+# Use date_delta as our feature and close as our target
+X = df['date_delta'].values.reshape(-1,1)
+y = df['close'].values.reshape(-1,1)
+
+# Split data into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Transform matrix of features into matrix of higher order (polynomial) features
+poly = PolynomialFeatures(degree = 4)
+X_poly = poly.fit_transform(X_train)
+
+# Fit into Linear Regression model
+model = LinearRegression()
+model.fit(X_poly, y_train)
+
+# Visualizing the Polynomial Regression results
+plt.scatter(X, y, color='grey')
+plt.plot(X, model.predict(poly.fit_transform(X)), color='blue')
+plt.title('Polynomial Prediction')
+plt.xlabel('Date')
+plt.ylabel('Stock CLOSE Price')
+plt.show()
