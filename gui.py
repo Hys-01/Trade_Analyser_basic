68421b1c68b583ab1150bd4b9d219458d6dde078
Trosn <110437947+Trosn@users.noreply.github.com>
2023-08-31 22:13:28 +1000

    gui updates, textbox and tab dimensions
    experiementing with dimensions, removed method from MovingAverage

--- gui.py
+++ gui.py
@@ -6,12 +6,16 @@ class MyTabView(customtkinter.CTkTabview):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-        # create tabs
-        self.add("tab 1")
-        self.add("tab 2")
+        # Tab creation with .add
+        self.add("t1")
+        self.add("t2")
 
         # add widgets on tabs
-        self.label = customtkinter.CTkLabel(master=self.tab("tab 1"))
+
+        # Label widget  
+        self.label = customtkinter.CTkLabel(master=self.tab("t1"))
+
+        # on the grid, can pad on x or y direction; unsure about row/column
         self.label.grid(row=0, column=0, padx=20, pady=10)
 
 
@@ -22,12 +26,12 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)  # configure grid system
         self.grid_columnconfigure(0, weight=1)
 
-        self.geometry("600x500")
+        self.geometry("800x800")    # W x H
         self.title("CTk example")
         
         
         # Tabview WIDGET 
-        self.tab_view = MyTabView(master=self)
+        self.tab_view = MyTabView(master=self, width=800, height=600)    # can enter arguments for width/length of tab
         self.tab_view.grid(row=0, column=0, padx=20, pady=20)
 
 
