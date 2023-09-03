8d4aad100089bb302e3fa0a7899523eea5f81776
Trosn <110437947+Trosn@users.noreply.github.com>
2023-09-03 14:30:36 +1000

    got graph onto gui tab
    looks weird, need to optimise or find a newer library

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from MovingAverage import MovingAverage as MA
 import numpy as np
 from config import API_KEY
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
 
 customtkinter.set_default_color_theme("green")
 
@@ -26,10 +27,10 @@ class MyTabView(customtkinter.CTkTabview):
 
 
     def tabGraph(self): 
-        plt.style.use('_mpl-gallery')
+        plt.style.use('seaborn-v0_8-pastel')
 
         mdf = MA(API_KEY)
-        mdf.retrieve_data('2020-01-01', '2023-01-01', 'TSLA')
+        mdf.retrieve_data('2020-01-01', '2023-01-01', 'AAPL')
         mdf.prepare_data()
         
         # creating a scatterplot of closing prices
@@ -37,11 +38,12 @@ class MyTabView(customtkinter.CTkTabview):
         x = mdf.data['date']
         y = mdf.data['close']
 
-        ax.scatter(x,y,label='Closing Prices', color='cyan', marker='o')
+        ax.scatter(x,y,label='Closing Prices', color='blue', marker='.')
 
-        plt.show()
 
-        canvas = customtkinter.CTkCanvas(master=self.tab("Graph"))
+        canvas = FigureCanvasTkAgg(fig, master=self.tab("Graph"))
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(fill=customtkinter.BOTH, expand=True)
 
 
 
@@ -53,12 +55,12 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)  # configure grid system
         self.grid_columnconfigure(0, weight=1)
 
-        self.geometry("800x700")    # W x H
+        self.geometry("800x800")    # W x H
         self.title("Trade Analyser Draft")
         
         
         # Tabview WIDGET 
-        self.tab_view = MyTabView(master=self, width=800, height=600)    # can enter arguments for width/length of tab
+        self.tab_view = MyTabView(master=self, width=800, height=700)    # can enter arguments for width/length of tab
         self.tab_view.grid(row=0, column=0, padx=20, pady=20)
 
 
