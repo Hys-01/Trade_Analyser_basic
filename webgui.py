3016b1a232880466410aaf4fd04244856cccec4b
Brian Hwang <hbros2004@gmail.com>
2023-09-16 17:30:51 +1000

    still not working

--- webgui.py
+++ webgui.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template 
 from MovingAverage import MovingAverage
 from gui import App
-webgui = Flask(__name__)  # initilalise the flask application
+webgui = Flask(__name__, template_folder='.')  # initilalise the flask application
 
-
+def load(): 
+    return render_template('web.html')
 if __name__ == "__main__":
     webgui.run() # run it
 
