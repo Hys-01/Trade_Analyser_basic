1ee8d2f7c4c5cb3fe6b6dc8799a9f8bf7fb2cef6
Brian Hwang <hbros2004@gmail.com>
2023-10-05 20:27:35 +1100

    needed help from chatgpt to open html
    this provides a http:ip that i copypaste into a browser. Doesnt open the app. Need tweaking.

--- webgui.py
+++ webgui.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template 
-from MovingAverage import MovingAverage
-from gui import App
+
 webgui = Flask(__name__, template_folder='.')  # initilalise the flask application
 
+@webgui.route('/')
 def load(): 
     return render_template('web.html')
 if __name__ == "__main__":
