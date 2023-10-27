6229405cc319c7863906ed86eae28a893f435d24
Brian Hwang <hbros2004@gmail.com>
2023-10-27 13:40:28 +1100

    setup for user input possibly

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from gui import App
 import pandas as pd
+import customtkinter as ctk
+import pandas as pd 
+
 
 # running the app
 app = App('2023-01-01', pd.Timestamp.today().date(), inputsymbol='NVDA')
