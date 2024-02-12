e71b2d084d22956ce69fff8a7550d6ca8efbe7a1
Hys-01 <hbros2004@gmail.com>
2024-02-12 22:05:48 +1100

    include company name

--- gui.py
+++ gui.py
@@ -101,17 +101,22 @@ class MyTabView(ctk.CTkTabview):
         # on the grid, can pad on x or y direction
         self.label.grid(row=0, column=0, padx=20, pady=10)
 
-        datetext = f"start point: {self.startD} \n end point: {self.todayD}"
+        datetext = f"start date: {self.startD} \n end date: {self.todayD}"
         
         self.labeldates = ctk.CTkLabel(master=self.tab("Menu"))
         self.labeldates.configure(text=datetext, padx = "100", font=(None,20))
         # on the grid, can pad on x or y direction
         self.labeldates.grid(row=2, column=0, padx=20, pady=10)
 
-        symtext = f"stock symbol: {self.symbol}"
+        symtext = f"NASDAQ: {self.symbol}"
         self.symbol_label = ctk.CTkLabel(master = self.tab('Menu'))
         self.symbol_label.configure(text = symtext, padx = "100", font=(None,20))
         self.symbol_label.grid(row=4, column=0, padx=20, pady=10)
+
+        symtext2 = f"Company: {'NVIDIA'}"
+        self.symbol_label = ctk.CTkLabel(master = self.tab('Menu'))
+        self.symbol_label.configure(text = symtext2, padx = "100", font=(None,20))
+        self.symbol_label.grid(row=5, column=0, padx=20, pady=10)
 
 
 
