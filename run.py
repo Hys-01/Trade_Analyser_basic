2440430ac959dfa6be7f47fa138f788ba3cfbbd0
Brian Hwang <hbros2004@gmail.com>
2024-03-06 12:14:17 +1100

    todo implement symbol input system
    -changed hardcoded symbol to cmd input for now

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 start_date = '2023-01-01'
 current_date = pd.Timestamp.today().date()
-inputsymbol = 'NVDA'
+
+# TODO: make a better input system, like a previous popup that asks for symbol (and) date?
+inputsymbol = input("symbol: ")  
 # running the app
 app = App(start_date, current_date, inputsymbol)
 
